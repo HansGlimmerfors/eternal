725a506a2b02f3ceb4dd2551bd4361132776b4b5
Hans Glimmerfors <hans@boardclic.com>
2025-06-14 20:16:31 +0200

    Add a missing test

---
+++ test/eternal/supervisor_test.exs
@@ -0,0 +1,13 @@
+defmodule Eternal.SupervisorTest do
+	use ExUnit.Case
+
+	alias Eternal.Supervisor
+
+  describe "start_link/3" do
+    test "ensure clashes are detected" do
+      assert {:ok, pid, :supervisor_test_cache} = Supervisor.start_link(:supervisor_test_cache)
+
+      assert {:error, {:already_started, ^pid}} = Supervisor.start_link(:supervisor_test_cache)
+    end
+  end
+end
